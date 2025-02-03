25d639661d7f331a6cf6791f3b3b504b71a9141b
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2025-02-03 21:14:19 +0400

    Add project files.

---
+++ MyQuizApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+
+namespace MyQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question(
+                    "What is the capital of Georgia?",
+                     new string[] { "Kutaisi", "Tbilisi", "Batumi", "Rustavi" }, 
+                     1
+                ), new Question(
+                    "What is 2 * 2?",
+                     new string[] { "3", "4", "6", "8" }, 
+                     1
+                ), new Question(
+                    "Which one is better?",
+                     new string[] { "BMW", "Subaru", "Chevy", "Audi" }, 
+                     2
+                )
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyQuizApp/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+
+        public string[] Answers { get; set; }
+
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string questionText, string[] anwers, int correctAnswerIndex)
+        {
+            QuestionText = questionText;
+            Answers = anwers;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+
+        public bool IsCorrectAnswer(int choice)
+        {
+            return CorrectAnswerIndex == choice;
+        }
+    }
+}
